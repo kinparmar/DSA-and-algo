7580a8116b4ecb9e74b77822eba53af796c61d60
kinparmar <119789351+kinparmar@users.noreply.github.com>
2022-12-04 12:20:49 +0100

    Create bst_array.cpp
    Binary search tree using array.
    
    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its number or index. Otherwise, return -1.
    
    You must write an algorithm with O(log n) runtime complexity.
    
     
    
    Example 1:
    
    Input: = [11,23,44,52,68,76], target = 76
    Output: findElement = 76
    Explanation: 76  exists in arrayand its index is 5
    
    Constraints:
    
    
    All the integers in nums are unique.
    nums is sorted in ascending order.

---
+++ src/bst_array/bst_array.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    int numElement=6;
+    int binaryLoop=numElement/2;
+    int array[] = {11,23,44,52,68,76};
+    int findElement = 76;
+    int *leftArray = &array[0]; 
+    int *rightArray = &array[numElement-1];
+    int *midArray  = &array[(numElement-1)/2];
+    int currentMid = (numElement-1)/2;
+    
+    cout<< "leftArray = "<<*leftArray<< " midArray= "<<*midArray<<" rightArray="<<*rightArray<<endl;
+   
+
+    for (int i=0; i<=binaryLoop; i++)
+    {
+       if(*midArray<findElement)
+       {
+           leftArray = &array[currentMid+1]; 
+           currentMid = currentMid + (numElement-1)/2;
+           midArray =  &array[currentMid+1];
+           cout<< "leftArray = "<<*leftArray<< " midArray= "<<*midArray<<" rightArray="<<*rightArray<<endl;
+           
+       }
+       else if(*midArray==findElement)
+       {
+            cout<< "findElement mid" << *midArray << endl;
+       }
+       else
+       {
+           cout<< "findElement left" << *leftArray << endl;+
+           
+       }
+    }
+    
+    return 0;
+}
